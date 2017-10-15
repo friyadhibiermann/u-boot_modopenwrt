b5d0b6743e270ab4604c218ba2a1fd7aee9a125d
friyadhibiermann <friyadhibiermann@gmail.com>
2017-10-15 05:29:43 -0400

    first commit

---
+++ u-boot/include/config.h
@@ -0,0 +1,19 @@
+#include <soc/soc_list.h>
+#define CONFIG_BUILD_DATE_UTC			2017-10-12
+#define CONFIG_WASP				1
+#define CONFIG_WASP_SUPPORT			1
+#define CONFIG_LSDK_KERNEL			1
+#define CONFIG_HOSTNAME				u-boot_tl-mr3420-v2
+#define CONFIG_GPIO_RESET_BTN			17
+#define CONFIG_GPIO_RESET_BTN_ACTIVE_LOW	1
+#define CONFIG_MAX_UBOOT_SIZE			125952
+#define CONFIG_MAX_UBOOT_SIZE_HEX		0x1EC00
+#define SOC_TYPE				QCA_AR9341_SOC
+#define CONFIG_BOARD_CUSTOM_STRING		TP-Link TL-MR3420 v2
+#define CONFIG_DEFAULT_FLASH_SIZE_IN_MB		4
+#define CONFIG_FOR_TPLINK_MR3420_V2		1
+#define CFG_ATHRS27_PHY				1
+#define CFG_AG7240_NMACS			2
+
+/* Automatically generated - do not edit */
+#include <configs/db12x.h>
